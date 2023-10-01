b9ffbd1379e810b107680dd74d8dfdc27255cb9d
Youngjin Shin <yjshin229@gmail.com>
2023-09-30 21:36:12 -0400

    feat: implement random num

--- hw04/SlotMachineSimulation7.10/Form1.Designer.cs
+++ hw04/SlotMachineSimulation7.10/Form1.Designer.cs
@@ -28,12 +28,146 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            button1 = new Button();
+            button2 = new Button();
+            label1 = new Label();
+            inserted_amount = new TextBox();
+            imageList1 = new ImageList(components);
+            pictureBox1 = new PictureBox();
+            pictureBox2 = new PictureBox();
+            pictureBox3 = new PictureBox();
+            won_text = new Label();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).BeginInit();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(220, 338);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 23);
+            button1.TabIndex = 0;
+            button1.Text = "Spin";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(301, 338);
+            button2.Name = "button2";
+            button2.Size = new Size(75, 23);
+            button2.TabIndex = 1;
+            button2.Text = "Exit";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(184, 298);
+            label1.Name = "label1";
+            label1.Size = new Size(111, 15);
+            label1.TabIndex = 2;
+            label1.Text = "Amount Inserted: $";
+            // 
+            // inserted_amount
+            // 
+            inserted_amount.Location = new Point(301, 295);
+            inserted_amount.Name = "inserted_amount";
+            inserted_amount.Size = new Size(100, 23);
+            inserted_amount.TabIndex = 3;
+            // 
+            // imageList1
+            // 
+            imageList1.ColorDepth = ColorDepth.Depth8Bit;
+            imageList1.ImageStream = (ImageListStreamer)resources.GetObject("imageList1.ImageStream");
+            imageList1.TransparentColor = Color.Transparent;
+            imageList1.Images.SetKeyName(0, "slot-machine-gem-svgrepo-com.png");
+            imageList1.Images.SetKeyName(1, "slot-machine-svgrepo-com.png");
+            imageList1.Images.SetKeyName(2, "slot-machine-citrus-svgrepo-com.png");
+            imageList1.Images.SetKeyName(3, "slot-machine-fruit-svgrepo-com.png");
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.BorderStyle = BorderStyle.FixedSingle;
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.InitialImage = (Image)resources.GetObject("pictureBox1.InitialImage");
+            pictureBox1.Location = new Point(102, 106);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(100, 141);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.TabIndex = 4;
+            pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.BorderStyle = BorderStyle.FixedSingle;
+            pictureBox2.Image = (Image)resources.GetObject("pictureBox2.Image");
+            pictureBox2.InitialImage = (Image)resources.GetObject("pictureBox2.InitialImage");
+            pictureBox2.Location = new Point(252, 106);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(100, 141);
+            pictureBox2.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox2.TabIndex = 5;
+            pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            pictureBox3.BorderStyle = BorderStyle.FixedSingle;
+            pictureBox3.Image = (Image)resources.GetObject("pictureBox3.Image");
+            pictureBox3.InitialImage = (Image)resources.GetObject("pictureBox3.InitialImage");
+            pictureBox3.Location = new Point(402, 106);
+            pictureBox3.Name = "pictureBox3";
+            pictureBox3.Size = new Size(100, 141);
+            pictureBox3.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox3.TabIndex = 6;
+            pictureBox3.TabStop = false;
+            // 
+            // won_text
+            // 
+            won_text.AutoSize = true;
+            won_text.Font = new Font("Malgun Gothic", 18F, FontStyle.Regular, GraphicsUnit.Point);
+            won_text.Location = new Point(233, 46);
+            won_text.Name = "won_text";
+            won_text.Size = new Size(131, 32);
+            won_text.TabIndex = 7;
+            won_text.Text = "Press Spin!";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(602, 430);
+            Controls.Add(won_text);
+            Controls.Add(pictureBox3);
+            Controls.Add(pictureBox2);
+            Controls.Add(pictureBox1);
+            Controls.Add(inserted_amount);
+            Controls.Add(label1);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Name = "Form1";
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Button button1;
+        private Button button2;
+        private Label label1;
+        private TextBox inserted_amount;
+        private ImageList imageList1;
+        private PictureBox pictureBox1;
+        private PictureBox pictureBox2;
+        private PictureBox pictureBox3;
+        private Label won_text;
     }
 }
--- hw04/SlotMachineSimulation7.10/Form1.cs
+++ hw04/SlotMachineSimulation7.10/Form1.cs
@@ -1,3 +1,5 @@
+using System.Globalization;
+
 namespace SlotMachineSimulation7._10
 {
     public partial class Form1 : Form
@@ -6,5 +8,81 @@ namespace SlotMachineSimulation7._10
         {
             InitializeComponent();
         }
+
+        private int[] Get_Three_Random()
+        {
+            int[] result = new int[3];
+            Random rand = new Random();
+            result[0] = rand.Next(4);
+            result[1] = rand.Next(4);
+            result[2] = rand.Next(4);
+            return result;
+        }
+
+        private int Check_Win(int[] arr)
+        {
+            int result = 0;
+            int current = 5;
+            foreach (int i in arr)
+            {
+                if (i == 0)
+                {
+                    current = i;
+                    result++;
+                }
+                else
+                {
+                    if (current == i)
+                    {
+                        result++;
+                    }
+                    else
+                    {
+                        current = i;
+                        result = 1;
+                    }
+                }
+            }
+            return result;
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(inserted_amount.Text))
+            {
+                MessageBox.Show("Put your bid to play", "Error");
+                return;
+            }
+            int[] three_random_num = Get_Three_Random();
+            int match = Check_Win(three_random_num);
+            decimal inserted = decimal.Parse(inserted_amount.Text);
+            pictureBox1.Image = imageList1.Images[three_random_num[0]];
+            pictureBox2.Image = imageList1.Images[three_random_num[1]];
+            pictureBox3.Image = imageList1.Images[three_random_num[2]];
+
+            switch (match)
+            {
+                case 1:
+                    won_text.Text = 0.ToString("C", CultureInfo.CreateSpecificCulture("en-US"));
+                    won_text.ForeColor = Color.Red;
+                    break;
+                case 2:
+                    won_text.Text = (inserted*2).ToString("C", CultureInfo.CreateSpecificCulture("en-US"));
+                    won_text.ForeColor= Color.Yellow;
+                    break;
+                case 3:
+                    won_text.Text = (inserted * 3).ToString("C", CultureInfo.CreateSpecificCulture("en-US"));
+                    won_text.ForeColor = Color.Yellow;
+                    break;
+                default:
+                    break;
+
+            }
+
+        }   
     }
 }
