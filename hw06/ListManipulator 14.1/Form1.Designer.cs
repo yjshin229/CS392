32d6d0221732a32e934eaf090178449893de561e
smokethecookie <ackura@bu.edu>
2023-10-18 17:53:17 -0400

    14.1 Implemented The random.txt file is inside bin/Debug

---
+++ hw06/ListManipulator 14.1/Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace ListManipulator_14._1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ListBoxNumbers = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // ListBoxNumbers
+            // 
+            this.ListBoxNumbers.FormattingEnabled = true;
+            this.ListBoxNumbers.ItemHeight = 25;
+            this.ListBoxNumbers.Location = new System.Drawing.Point(128, 96);
+            this.ListBoxNumbers.Name = "ListBoxNumbers";
+            this.ListBoxNumbers.Size = new System.Drawing.Size(431, 179);
+            this.ListBoxNumbers.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.ListBoxNumbers);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox ListBoxNumbers;
+    }
+}
+
---
+++ hw06/ListManipulator 14.1/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace ListManipulator_14._1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            List<int> numbers = new List<int>();
+
+            string[] lines = File.ReadAllLines("random.txt");
+            foreach (string line in lines)
+            {
+                if (int.TryParse(line, out int number))
+                {
+                    numbers.Add(number);
+                }
+            }
+            numbers.RemoveAll(n => n < 0);
+            List<int> filteredNumbers = numbers.FindAll(n => n >= 1 && n <= 10);
+            ListBoxNumbers.DataSource = filteredNumbers;
+        }
+    }
+}
